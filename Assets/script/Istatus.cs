3a1343c692a83033b1898da28840fccf8277648a
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2023-06-01 18:11:09 +0900

    [add]Script _interface_Istatus,class_damage

---
+++ Assets/script/Istatus.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Istatus
+{
+    public void Damage(int value);
+    public void Death();
+    public void Electric(int value);
+}
