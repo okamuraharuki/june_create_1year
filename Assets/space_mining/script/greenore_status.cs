4573871fdef93ed19ead180c1d0cdca1a7c97d30
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2023-06-13 04:43:09 +0900

    [Add]animation_mineの追加

---
+++ Assets/space_mining/script/greenore_status.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class greenore_status : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/space_mining/script/scene_manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene_manager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
